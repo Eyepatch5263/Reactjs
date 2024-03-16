2cf6bfaed35ba02e8d0a6265c9e428460220c19e
Eyepatch <pratyushpragyey@gmail.com>
2024-03-16 14:29:04 +0530

    ToDoList

---
+++ TodoList/src/context/ToDoContext.js
@@ -0,0 +1,23 @@
+import { createContext,useContext } from "react";
+
+export const ToDoContext=createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo Message",
+            completed:false,
+        }
+    ],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+});
+
+
+export const UseToDo=()=>{
+    return useContext(ToDoContext);
+}
+
+export const TodoProvider=ToDoContext.Provider
+
